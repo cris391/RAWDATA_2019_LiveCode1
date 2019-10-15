d2dc356e9e39880abb4cd26b1802c237ad2c7dec
Henrik Bulskov Styltsvig <bulskov@ruc.dk>
2019-10-15 10:56:15 +0200

    Extension methods!

--- LiveCode1/Program.cs
+++ LiveCode1/Program.cs
@@ -1,12 +1,27 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace LiveCode1
 {
+
+    static class MyExt
+    {
+        public static string AsString<T>(this IEnumerable<T> a)
+        {
+            return string.Join(", ", a);
+        } 
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            
+            var a = new int[]{1,12,3,4,5};
+
+            var aAsString = string.Join(", ", a);
+
+            Console.WriteLine(a.AsString());
+
         }
     }
 }
